d1ad5761f785470f381d9b8b98ebbcb0afd0fab2
PSK <59411218+psk488@users.noreply.github.com>
2022-03-29 19:10:39 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import './App.css';
+import { BrowserRouter as Router,Routes, Route, Switch } from "react-router-dom";
+import Home from './pages/Home';
+import Menu from './pages/Menu';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import About from './pages/About';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>    
+        <Navbar />
+      <Routes>           
+            <Route path='/' exact element={<Home />} />          
+            <Route path='/menu' exact element={<Menu />} />          
+            <Route path='/about' exact element={<About />} />          
+      </Routes>
+      <Footer />
+      </Router>
+    
     </div>
   );
 }
